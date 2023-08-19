e2ca199c8d5be5c1a63613f8237387c836f13400
DontSleep404 <80206409+dontwannasleep@users.noreply.github.com>
2023-08-19 18:55:15 +0700

    Update a.js

--- a.js
+++ a.js
@@ -1,7 +1,1 @@
-// @name Alert test
-// @description Displays an alert dialog on google.it
-// @author Emanuele Faranda
-// @version 1.0
-// @match *://system.cheat-aov.net/lgl-protect/login-lgl.php
-
-alert("Js Injector works");
+yaaX85VmkQGzYQYAdMH
